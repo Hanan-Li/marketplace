d7f7ce05bd18290cb8af557055742962d611de25
Hanan Li <hananli@gatech.edu>
2021-04-01 16:45:25 -0400

    Initial Electron App

---
+++ src/ownshop.js
@@ -0,0 +1,32 @@
+// Javascript for main page
+const  create  = require('ipfs-http-client');
+const ipfs = create();
+let PeerID = '';
+ipfs.config.getAll();
+// Function to get Peer ID
+async function getPeerId() {
+    const config = await ipfs.config.getAll();
+    PeerID = config['Identity']['PeerID'];
+}
+
+// Add Item to IPFS
+function addNewItem(e) {
+    e.preventDefault();
+    const itemName = document.getElementById('ItemName').value;
+    const itemPrice = document.getElementById('ItemPrice').value;
+    console.log(itemName);
+    console.log(itemPrice);
+    console.log("hihi")
+}
+
+// Set PeerID and other information
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+    getPeerId().then(result => {
+        replaceText('PeerId', PeerID);
+    });
+    
+  })
