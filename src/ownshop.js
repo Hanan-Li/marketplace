f5cfeaa5854f902d9fcde3b84d4be28100f0330e
Hanan Li <hananli@gatech.edu>
2021-04-17 20:09:17 -0400

    mostly done

--- src/ownshop.js
+++ src/ownshop.js
@@ -28,88 +28,88 @@ async function addNewItem(e) {
 }
 
 // Search Item
-async function searchItem(e) {
-    e.preventDefault();
-    // clean up search item card
-    for (let index = 0; index < 3; index++) {
-        document.getElementById("sith" + index).innerText = "No Item";
-        document.getElementById("sitp" + index).innerText = "No Item";
-        document.getElementById("sits" + index).innerText = "No Item";
-    }
-    const searchItemName = document.getElementById('searchItemName').value;
-    console.log(searchItemName);
-    const candidate = new Set();
-    let itemFound = 0;
-    for (let store of knownStore) {
-        console.log(store);
-        // console.log(ipfs.cat('/ipns/' + store));
-        // console.log(ipfs.name.resolve('/ipns/' + store));
-        // for await (const name of ipfs.name.resolve('/ipns/' + store)) {
-        //     console.log(name)
-        // }
-        const chunks = []
-        for await (const chunk of ipfs.cat('/ipns/' + store)) {
-            chunks.push(chunk);
-        }
-        const items = JSON.parse(Buffer.concat(chunks).toString());
-        console.log(items)
-        for (let item of items['items']) {
-            console.log(item.name);
-            if (item.name == searchItemName) {
-                candidate.add(store);
-                console.log('find item');
-                document.getElementById("sith" + itemFound).innerText = item.name;
-                document.getElementById("sitp" + itemFound).innerText = item.price;
-                document.getElementById("sits" + itemFound).innerText = store;
-                itemFound = itemFound + 1;
-            }
-        }
-    }
-    // publish the search query to others under the subscription
-    const msg = new TextEncoder().encode('search:' + searchItemName + ' ' + fileID)
-    await ipfs.pubsub.publish(topic, msg)
-}
+// async function searchItem(e) {
+//     e.preventDefault();
+//     // clean up search item card
+//     for (let index = 0; index < 3; index++) {
+//         document.getElementById("sith" + index).innerText = "No Item";
+//         document.getElementById("sitp" + index).innerText = "No Item";
+//         document.getElementById("sits" + index).innerText = "No Item";
+//     }
+//     const searchItemName = document.getElementById('searchItemName').value;
+//     console.log(searchItemName);
+//     const candidate = new Set();
+//     let itemFound = 0;
+//     for (let store of knownStore) {
+//         console.log(store);
+//         // console.log(ipfs.cat('/ipns/' + store));
+//         // console.log(ipfs.name.resolve('/ipns/' + store));
+//         // for await (const name of ipfs.name.resolve('/ipns/' + store)) {
+//         //     console.log(name)
+//         // }
+//         const chunks = []
+//         for await (const chunk of ipfs.cat('/ipns/' + store)) {
+//             chunks.push(chunk);
+//         }
+//         const items = JSON.parse(Buffer.concat(chunks).toString());
+//         console.log(items)
+//         for (let item of items['items']) {
+//             console.log(item.name);
+//             if (item.name == searchItemName) {
+//                 candidate.add(store);
+//                 console.log('find item');
+//                 document.getElementById("sith" + itemFound).innerText = item.name;
+//                 document.getElementById("sitp" + itemFound).innerText = item.price;
+//                 document.getElementById("sits" + itemFound).innerText = store;
+//                 itemFound = itemFound + 1;
+//             }
+//         }
+//     }
+//     // publish the search query to others under the subscription
+//     const msg = new TextEncoder().encode('search:' + searchItemName + ' ' + fileID)
+//     await ipfs.pubsub.publish(topic, msg)
+// }
 
-// Rate Item
-async function rateItem(elem) {
-    console.log("rate item");
-    var idx = elem.value;
-    console.log(idx);
-    var item = document.getElementById("sith" + idx).innerText;
-    console.log(item);
-    var itemStore = document.getElementById("sits" + idx).innerText;
-    console.log(itemStore);
-    var rating = document.getElementById("rate" + idx).value;
-    console.log(rating);
-    if (item !== "No Item" && rating !== "") {
-        console.log("record rating");
-        if (storeInfo['scores'].has(itemStore) == false) {
-            storeInfo['scores'].set(itemStore, parseInt(rating));
-        }
-        else {
-            storeInfo['scores'].set(itemStore, storeInfo['scores'].get(itemStore) + parseInt(rating));
-        }
-        console.log(storeInfo['scores']);
-        console.log(storeInfo);
-        const fs = require('fs');
-        fs.writeFile(fileAddress, JSON.stringify(storeInfo, replacer), (err) => {
-            if (err) throw err;
-        });
-    }
-}
+// // Rate Item
+// async function rateItem(elem) {
+//     console.log("rate item");
+//     var idx = elem.value;
+//     console.log(idx);
+//     var item = document.getElementById("sith" + idx).innerText;
+//     console.log(item);
+//     var itemStore = document.getElementById("sits" + idx).innerText;
+//     console.log(itemStore);
+//     var rating = document.getElementById("rate" + idx).value;
+//     console.log(rating);
+//     if (item !== "No Item" && rating !== "") {
+//         console.log("record rating");
+//         if (storeInfo['scores'].has(itemStore) == false) {
+//             storeInfo['scores'].set(itemStore, parseInt(rating));
+//         }
+//         else {
+//             storeInfo['scores'].set(itemStore, storeInfo['scores'].get(itemStore) + parseInt(rating));
+//         }
+//         console.log(storeInfo['scores']);
+//         console.log(storeInfo);
+//         const fs = require('fs');
+//         fs.writeFile(fileAddress, JSON.stringify(storeInfo, replacer), (err) => {
+//             if (err) throw err;
+//         });
+//     }
+// }
 
 
 
-function replacer(key, value) {
-    if (value instanceof Map) {
-        return {
-            dataType: 'Map',
-            value: Array.from(value.entries()),
-        };
-    } else {
-        return value;
-    }
-}
+// function replacer(key, value) {
+//     if (value instanceof Map) {
+//         return {
+//             dataType: 'Map',
+//             value: Array.from(value.entries()),
+//         };
+//     } else {
+//         return value;
+//     }
+// }
 
 function updateItems(storeInfo) {
     
